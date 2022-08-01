4202082a8f1f225729cc384d9a8fdc5ccfcbfdbc
sandeep625858 <108717062+sandeep625858@users.noreply.github.com>
2022-08-01 07:49:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,18 +7,34 @@ const url=[
     "https://cdn.shopify.com/s/files/1/0057/8938/4802/files/A121-website-banner_2000x.jpg?v=1658560096",
     "https://cdn.shopify.com/s/files/1/0057/8938/4802/files/banner-web_2000x.jpg?v=1659119373"];
 next.addEventListener('click',()=>{
+    goToSlideN();
+    clearInterval(slideInterval);
+})
+prev.addEventListener('click',()=>{
+    goToSlideP();
+    clearInterval(slideInterval1);
+})
+function goToSlideN()
+{
     num++;
     if(num==url.length)
     {
         num=0;
     }
     allimages.innerHTML='<img src='+url[num]+'class="image" id="im"></img>';
-})
-prev.addEventListener('click',()=>{
+}
+function goToSlideP()
+{
     num--;
     if(num<0)
     {
         num=url.length-1;
     }
     allimages.innerHTML='<img src='+url[num]+'class="image" id="im"></img>';
-})
+}
+const slideInterval = setInterval(() => {
+    goToSlideN();
+  }, 3000);
+  const slideInterval1 = setInterval(() => {
+    goToSlideP();
+  }, 3000);
